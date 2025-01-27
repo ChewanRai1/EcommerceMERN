f2f137a3dc3f7cf3f794350403334a17c23e5469
ChewanRai1 <220154@softwarica.edu.np>
2025-01-27 18:31:24 +0545

    user controller password

--- server-app/controllers/user.js
+++ server-app/controllers/user.js
@@ -1,10 +1,13 @@
 //[SECTION] Dependencies and Modules
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const User = require("../models/User");
 const Enrollment = require("../models/Enrollment");
 const auth = require("../auth");
 
 const { errorHandler } = auth;
+
+// Define password complexity regex
+const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
 
 //[SECTION] Check if the email already exists
 /*
@@ -13,21 +16,19 @@ const { errorHandler } = auth;
     2. Use the "then" method to send a response back to the client appliction based on the result of the "find" method
 */
 module.exports.checkEmailExists = (req, res) => {
-
-    if(req.body.email.includes("@")){
-        return User.find({ email : req.body.email })
-        .then(result => {
-
-            if (result.length > 0) {
-                return res.status(409).send({ message: "Duplicate email found" });
-            } else {
-                return res.status(404).send({ message: "No duplicate email found" });
-            };
-        })
-        .catch(error => errorHandler(error, req, res));
-    } else{
-        res.status(400).send({ message: "Invalid email format" })
-    }
+  if (req.body.email.includes("@")) {
+    return User.find({ email: req.body.email })
+      .then((result) => {
+        if (result.length > 0) {
+          return res.status(409).send({ message: "Duplicate email found" });
+        } else {
+          return res.status(404).send({ message: "No duplicate email found" });
+        }
+      })
+      .catch((error) => errorHandler(error, req, res));
+  } else {
+    res.status(400).send({ message: "Invalid email format" });
+  }
 };
 
 //[SECTION] User registration
@@ -37,40 +38,89 @@ module.exports.checkEmailExists = (req, res) => {
     2. Make sure that the password is encrypted
     3. Save the new User to the database
 */
-module.exports.registerUser = (req, res) => {
+// module.exports.registerUser = (req, res) => {
+//   // Checks if the email is in the right format
+//   if (!req.body.email.includes("@")) {
+//     // if the email is not in the right format, send a message 'Invalid email format'.
+//     return res.status(400).send({ message: "Invalid email format" });
+//   }
+//   // Checks if the mobile number has the correct number of characters
+//   else if (req.body.mobileNo.length !== 10) {
+//     // if the mobile number is not in the correct number of characters, send a message 'Mobile number is invalid'.
+//     return res.status(400).send({ message: "Mobile number is invalid" });
+//   }
+//   // Checks if the password has atleast 8 characters
+//   else if (req.body.password.length < 8) {
+//     // If the password is not atleast 8 characters, send a message 'Password must be atleast 8 characters long'.
+//     return res
+//       .status(400)
+//       .send({ message: "Password must be atleast 8 characters long" });
+//     // If all needed requirements are achieved
+//   } else {
+//     let newUser = new User({
+//       firstName: req.body.firstName,
+//       lastName: req.body.lastName,
+//       email: req.body.email,
+//       mobileNo: req.body.mobileNo,
+//       password: bcrypt.hashSync(req.body.password, 10),
+//     });
 
-    // Checks if the email is in the right format
-    if (!req.body.email.includes("@")){
-        // if the email is not in the right format, send a message 'Invalid email format'.
-        return res.status(400).send({ message: 'Invalid email format' });
-    }
-    // Checks if the mobile number has the correct number of characters
-    else if (req.body.mobileNo.length !== 10){
-        // if the mobile number is not in the correct number of characters, send a message 'Mobile number is invalid'.
-        return res.status(400).send({ message: 'Mobile number is invalid' });
-    }
-    // Checks if the password has atleast 8 characters
-    else if (req.body.password.length < 8) {
-        // If the password is not atleast 8 characters, send a message 'Password must be atleast 8 characters long'.
-        return res.status(400).send({ message: 'Password must be atleast 8 characters long' });
-    // If all needed requirements are achieved
-    } else {
-        let newUser = new User({
-            firstName : req.body.firstName,
-            lastName : req.body.lastName,
-            email : req.body.email,
-            mobileNo : req.body.mobileNo,
-            password : bcrypt.hashSync(req.body.password, 10)
-        })
+//     return (
+//       newUser
+//         .save()
+//         // if all needed requirements are achieved, send a success message 'User registered successfully' and return the newly created user.
+//         .then((result) =>
+//           res.status(201).send({
+//             message: "User registered successfully",
+//             user: result,
+//           })
+//         )
+//         .catch((error) => errorHandler(error, req, res))
+//     );
+//   }
+// };
 
-        return newUser.save()
-        // if all needed requirements are achieved, send a success message 'User registered successfully' and return the newly created user.
-        .then((result) => res.status(201).send({
-            message: 'User registered successfully',
-            user: result
-        }))
-        .catch(error => errorHandler(error, req, res));
-    }
+module.exports.registerUser = async (req, res) => {
+  const { firstName, lastName, email, mobileNo, password } = req.body;
+
+  // Email validation
+  if (!email.includes("@")) {
+    return res.status(400).send({ message: "Invalid email format" });
+  }
+
+  // Mobile number validation
+  if (mobileNo.length !== 10 || !/^\d+$/.test(mobileNo)) {
+    return res
+      .status(400)
+      .send({ message: "Mobile number must be 10 digits long" });
+  }
+
+  // Password complexity validation
+  if (!passwordRegex.test(password)) {
+    return res.status(400).send({
+      message:
+        "Password must contain at least 8 characters, including uppercase, lowercase, number, and special character.",
+    });
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      mobileNo,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+    res
+      .status(201)
+      .send({ message: "User registered successfully", user: newUser });
+  } catch (error) {
+    errorHandler(error, req, res);
+  }
 };
 
 //[SECTION] User authentication
@@ -81,32 +131,36 @@ module.exports.registerUser = (req, res) => {
     3. Generate/return a JSON web token if the user is successfully logged in and return false if not
 */
 module.exports.loginUser = (req, res) => {
-
-    if(req.body.email.includes("@")){
-        return User.findOne({ email : req.body.email })
-        .then(result => {
-            if(result == null){
-                // if the email is not found, send a message 'No email found'.
-                return res.status(404).send({ message: 'No email found' });
-            } else {
-                const isPasswordCorrect = bcrypt.compareSync(req.body.password, result.password);
-                if (isPasswordCorrect) {
-                    // if all needed requirements are achieved, send a success message 'User logged in successfully' and return the access token.
-                    return res.status(200).send({ 
-                        message: 'User logged in successfully',
-                        access : auth.createAccessToken(result)
-                        })
-                } else {
-                    // if the email and password is incorrect, send a message 'Incorrect email or password'.
-                    return res.status(401).send({ message: 'Incorrect email or password' });
-                }
-            }
-        })
-        .catch(error => errorHandler(error, req, res));
-    } else{
-        // if the email used in not in the right format, send a message 'Invalid email format'.
-        return res.status(400).send({ message: 'Invalid email format' });
-    }
+  if (req.body.email.includes("@")) {
+    return User.findOne({ email: req.body.email })
+      .then((result) => {
+        if (result == null) {
+          // if the email is not found, send a message 'No email found'.
+          return res.status(404).send({ message: "No email found" });
+        } else {
+          const isPasswordCorrect = bcrypt.compareSync(
+            req.body.password,
+            result.password
+          );
+          if (isPasswordCorrect) {
+            // if all needed requirements are achieved, send a success message 'User logged in successfully' and return the access token.
+            return res.status(200).send({
+              message: "User logged in successfully",
+              access: auth.createAccessToken(result),
+            });
+          } else {
+            // if the email and password is incorrect, send a message 'Incorrect email or password'.
+            return res
+              .status(401)
+              .send({ message: "Incorrect email or password" });
+          }
+        }
+      })
+      .catch((error) => errorHandler(error, req, res));
+  } else {
+    // if the email used in not in the right format, send a message 'Invalid email format'.
+    return res.status(400).send({ message: "Invalid email format" });
+  }
 };
 
 //[Section] Activity: Retrieve user details
@@ -117,51 +171,46 @@ module.exports.loginUser = (req, res) => {
     3. Return the updated user record
 */
 module.exports.getProfile = (req, res) => {
-    return User.findById(req.user.id)
-    .then(user => {
-
-        if(!user){
-            // if the user has invalid token, send a message 'invalid signature'.
-            return res.status(404).send({ message: 'invalid signature' })
-        }else {
-            // if the user is found, return the user.
-            user.password = "";
-            return res.status(200).send(user);
-        }  
+  return User.findById(req.user.id)
+    .then((user) => {
+      if (!user) {
+        // if the user has invalid token, send a message 'invalid signature'.
+        return res.status(404).send({ message: "invalid signature" });
+      } else {
+        // if the user is found, return the user.
+        user.password = "";
+        return res.status(200).send(user);
+      }
     })
-    .catch(error => errorHandler(error, req, res));
+    .catch((error) => errorHandler(error, req, res));
 };
 
-
-
 module.exports.enroll = (req, res) => {
+  console.log(req.user.id);
+  console.log(req.body.enrolledCourses);
 
-    console.log(req.user.id);
-    console.log(req.body.enrolledCourses) ;
+  if (req.user.isAdmin) {
+    // if the user is an admin, send a message 'Admin is forbidden'.
+    return res.status(403).send({ message: "Admin is forbidden" });
+  }
 
-    if(req.user.isAdmin){
-        // if the user is an admin, send a message 'Admin is forbidden'.
-        return res.status(403).send({ message: 'Admin is forbidden' });
-    }
+  let newEnrollment = new Enrollment({
+    userId: req.user.id,
+    enrolledCourses: req.body.enrolledCourses,
+    totalPrice: req.body.totalPrice,
+  });
 
-    let newEnrollment = new Enrollment ({
-        userId : req.user.id,
-        enrolledCourses: req.body.enrolledCourses,
-        totalPrice: req.body.totalPrice
+  return newEnrollment
+    .save()
+    .then((enrolled) => {
+      // if the user successfully enrolled,return true and send a message 'Enrolled successfully'.
+      return res.status(201).send({
+        success: true,
+        message: "Enrolled successfully",
+      });
     })
-
-    return newEnrollment.save()
-    .then(enrolled => {
-        // if the user successfully enrolled,return true and send a message 'Enrolled successfully'.
-        return res.status(201).send({
-            success: true,
-            message: 'Enrolled successfully'
-        });
-    })
-    .catch(error => errorHandler(error, req, res));
-    
-}
-
+    .catch((error) => errorHandler(error, req, res));
+};
 
 //[SECTION] Activity: Get enrollments
 /*
@@ -171,87 +220,78 @@ module.exports.enroll = (req, res) => {
 */
 
 module.exports.getEnrollments = (req, res) => {
-    return Enrollment.find({userId : req.user.id})
-        .then(enrollments => {
-            if (enrollments.length > 0) {
-                // if there are enrolled courses, return the enrollments.
-                return res.status(200).send(enrollments);
-            }
-            // if there is no enrolled courses, send a message 'No enrolled courses'.
-            return res.status(404).send({
-                message: 'No enrolled courses'
-            });
-        })
-        .catch(error => errorHandler(error, req, res));
+  return Enrollment.find({ userId: req.user.id })
+    .then((enrollments) => {
+      if (enrollments.length > 0) {
+        // if there are enrolled courses, return the enrollments.
+        return res.status(200).send(enrollments);
+      }
+      // if there is no enrolled courses, send a message 'No enrolled courses'.
+      return res.status(404).send({
+        message: "No enrolled courses",
+      });
+    })
+    .catch((error) => errorHandler(error, req, res));
 };
-
 
 //[SECTION] Reset password
 // Modify how we export our controllers
 module.exports.resetPassword = async (req, res) => {
+  try {
+    // Add a console.log() to check if you can pass data properly from postman
+    // console.log(req.body);
 
-    try {
+    // Add a console.log() to show req.user, our decoded token, does not contain userId property but instead id
+    // console.log(req.user);
 
-        // Add a console.log() to check if you can pass data properly from postman
-        // console.log(req.body);
+    const { newPassword } = req.body;
 
-        // Add a console.log() to show req.user, our decoded token, does not contain userId property but instead id
-        // console.log(req.user);
+    // update userId to id because our version of req.user does not have userId property but id property instead.
+    const { id } = req.user; // Extracting user ID from the authorization header
 
-        const { newPassword } = req.body;
+    // Hashing the new password
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
 
-        // update userId to id because our version of req.user does not have userId property but id property instead.
-        const { id } = req.user; // Extracting user ID from the authorization header
+    // Update userId update to id
+    // Updating the user's password in the database
+    await User.findByIdAndUpdate(id, { password: hashedPassword });
 
-        // Hashing the new password
-        const hashedPassword = await bcrypt.hash(newPassword, 10);
-        
-        // Update userId update to id
-        // Updating the user's password in the database
-        await User.findByIdAndUpdate(id, { password: hashedPassword });
-
-        // Sending a success response
-        res.status(200).json({ message: 'Password reset successfully' });
-
-    } catch (error) {
-
-        console.error(error);
-        res.status(500).json({ message: 'Internal server error' });
-        
-    }
-
+    // Sending a success response
+    res.status(200).json({ message: "Password reset successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
-
 
 //[SECTION] Update profile
 // Update the function to arrow to unify our code formats
 // Modify how we export our controllers
 module.exports.updateProfile = async (req, res) => {
-    try {
+  try {
+    // Add a console.log() to check if you can pass data properly from postman
+    // console.log(req.body);
 
-        // Add a console.log() to check if you can pass data properly from postman
-        // console.log(req.body);
+    // Add a console.log() to show req.user, our decoded token, does have id property
+    // console.log(req.user);
 
-        // Add a console.log() to show req.user, our decoded token, does have id property
-        // console.log(req.user);
-            
-        // Get the user ID from the authenticated token
-        const userId = req.user.id;
+    // Get the user ID from the authenticated token
+    const userId = req.user.id;
 
-        // Retrieve the updated profile information from the request body
-        // Update the req.body to use mobileNo instead of mobileNumber to match our schema
-        const { firstName, lastName, mobileNo } = req.body;
+    // Retrieve the updated profile information from the request body
+    // Update the req.body to use mobileNo instead of mobileNumber to match our schema
+    const { firstName, lastName, mobileNo } = req.body;
 
-        // Update the user's profile in the database
-        const updatedUser = await User.findByIdAndUpdate(
-            userId,
-            { firstName, lastName, mobileNo },
-            { new: true }
-        );
+    // Update the user's profile in the database
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { firstName, lastName, mobileNo },
+      { new: true }
+    );
 
-        res.send(updatedUser);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send({ message: 'Failed to update profile' });
-    }
-}
+    res.send(updatedUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: "Failed to update profile" });
+  }
+};

743dbd44b98c8bb2799db85c0f84109053529e7e
ChewanRai1 <220154@softwarica.edu.np>
2025-01-30 10:13:45 +0545

    logout and clearing jwt

--- server-app/routes/user.js
+++ server-app/routes/user.js
@@ -31,6 +31,9 @@ router.post("/enroll", verify, userController.enroll);
 // Route to get the user's enrollements array
 router.get("/getEnrollments", verify, userController.getEnrollments);
 
+// ✅ [MODIFIED] Secure Logout Route (Clears JWT & Session)
+router.get("/logout", auth.logoutUser);
+
 //[SECTION] Google Login
 //[SECTION] Route for initiating the Google OAuth consent screen
 router.get(
@@ -73,25 +76,27 @@ router.get("/success", isLoggedIn, (req, res) => {
   res.send(`Welcome ${req.user.displayName}`);
 });
 
+
+//This is through auth.js
 //[SECTION] Route for logging out of the application
 // The logout route does only logs the user out of the application and destroys the session, but upon trying to access the "/google" route again, the user is no longer prompted to choose an email to login if the "prompt : "select_account" option is not added to the "/google" route. This is expected behaviour because the Google OAuth 2, already allows the user access to the "Course Booking API" because the user has been authorized to access the app.
 // Navigate to the Google App Permissions to delete all connections with the app (https://myaccount.google.com/connections)
-router.get("/logout", (req, res) => {
-  // Destroys the session that stores the Google OAuth Client credentials
-  // Allows for release of resources when the account information is no longer needed in the browser
-  req.session.destroy((err) => {
-    if (err) {
-      console.log("Error while destroying session:", err);
-    } else {
-      req.logout(() => {
-        console.log("You are logged out");
-        // Redirects the page to "http://localhost:4000" route to visual redirection in frontend.
-        // Can be replaced in the future with the "home" page route for the frontend.
-        res.redirect("/");
-      });
-    }
-  });
-});
+// router.get("/logout", (req, res) => {
+//   // Destroys the session that stores the Google OAuth Client credentials
+//   // Allows for release of resources when the account information is no longer needed in the browser
+//   req.session.destroy((err) => {
+//     if (err) {
+//       console.log("Error while destroying session:", err);
+//     } else {
+//       req.logout(() => {
+//         console.log("You are logged out");
+//         // Redirects the page to "http://localhost:4000" route to visual redirection in frontend.
+//         // Can be replaced in the future with the "home" page route for the frontend.
+//         res.redirect("/");
+//       });
+//     }
+//   });
+// });
 
 //[SECTION] Route for resetting the user password
 router.put("/reset-password", verify, userController.resetPassword);

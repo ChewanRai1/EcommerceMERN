3e4683e8901ee8b62b749903b2c1e43bc515e26d
ChewanRai1 <220154@softwarica.edu.np>
2025-01-31 10:48:17 +0545

    forgot pws with ndoemailer

---
+++ frontend/src/components/ForgotPassword.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const ForgotPassword = () => {
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch(
+        `${import.meta.env.VITE_API_URL}/users/forgot-password`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ email }),
+        }
+      );
+
+      const data = await res.json();
+      setMessage(data.message);
+
+      if (res.ok) {
+        navigate("/verify-otp");
+      }
+    } catch {
+      setMessage("Error sending OTP.");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Forgot Password</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Enter your email"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button type="submit">Reset Password</button>
+      </form>
+      <p>{message}</p>
+    </div>
+  );
+};
+
+export default ForgotPassword;

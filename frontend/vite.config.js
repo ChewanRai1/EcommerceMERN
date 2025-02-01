d11b39a0a33c815d43bd7e4bffa7c516ba8f4ba2
ChewanRai1 <220154@softwarica.edu.np>
2025-02-01 10:00:36 +0545

    config modofied for ssl

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import fs from "fs";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "localhost",
+    port: 5173, // Change to any available port if needed
+    https: {
+      key: fs.readFileSync("../server-app/ssl/server.key"), // Use the same SSL certificate
+      cert: fs.readFileSync("../server-app/ssl/server.cert"),
+    },
+  },
+});
